87a2672f30fb0b04cc4cbcfdb4d8081fef162240
Michael Škvrňák <397925@mail.muni.cz>
2021-09-07 14:33:02 +0200

    Start working on video download script

---
+++ 01_scrape_ad_videos.R
@@ -0,0 +1,36 @@
+library(rvest)
+library(RSelenium)
+library(Radlibrary)
+
+access_token <- token_get()$token
+leaders_ads <- readRDS("output/leaders_ads_2021-09-01.RData")
+parties_ads <- readRDS("output/parties_ads_2021-09-01.RData")
+
+url <- glue::glue("https://www.facebook.com/ads/archive/render_ad/?id=367215521451359&access_token={access_token}")
+
+remDr <- remoteDriver(
+    remoteServerAddr = "localhost",
+    port = 4445L,
+    browserName = "firefox"
+)
+
+download_video <- function(remote_driver, url){
+    id <- stringr::str_extract(url, "(?<=id=)[0-9]+")
+    
+    remote_driver$open()
+    remote_driver$navigate(url)
+    html_source <- remote_driver$getPageSource()[[1]] %>%
+        read_html()
+    
+    video_src <- html_source %>%
+        html_node("video") %>%
+        html_attr("src")
+    
+    if(length(video_src)){
+        download.file(video_src, destfile = paste0(id, ".mp4"))
+    }
+    
+    remote_driver$close()
+}
+
+download_video(remDr, url)
--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 - `00_get_fbads_data.R` - download data from FB Ads Library about Czech parties and party leaders
 - `01_token_log_odds.R` - lemmatise the ads and visualise log odds
+- `01_scrape_ad_videos.R` - script for downloading videos in FB ads
 
 ![](output/charts/log-odds.png)
