42747437f3ad5ec31244323a087b284f1b3d9698
Michael Škvrňák <397925@mail.muni.cz>
2021-09-08 13:28:52 +0200

    Update scrape videos script

--- 01_scrape_ad_videos.R
+++ 01_scrape_ad_videos.R
@@ -3,10 +3,11 @@ library(RSelenium)
 library(Radlibrary)
 
 access_token <- token_get()$token
-# leaders_ads <- readRDS("output/leaders_ads_2021-09-01.RData")
+leaders_ads <- readRDS("output/leaders_ads_2021-09-08.RData")
 # parties_ads <- readRDS("output/parties_ads_2021-09-01.RData")
 
-url <- glue::glue("https://www.facebook.com/ads/archive/render_ad/?id=1040437186723679&access_token={access_token}")
+babis <- leaders_ads %>%
+    filter(page_name == "Andrej Babiš")
 
 remDr <- remoteDriver(
     remoteServerAddr = "localhost",
@@ -14,7 +15,7 @@ remDr <- remoteDriver(
     browserName = "firefox"
 )
 
-download_video <- function(remote_driver, url){
+download_video <- function(remote_driver, url, folder = "videos/"){
     id <- stringr::str_extract(url, "(?<=id=)[0-9]+")
     
     remote_driver$open()
@@ -26,11 +27,20 @@ download_video <- function(remote_driver, url){
         html_node("video") %>%
         html_attr("src")
     
-    if(length(video_src)){
-        download.file(video_src, destfile = paste0(id, ".mp4"))
+    if(length(video_src) & !is.na(video_src)){
+        download.file(video_src, destfile = paste0(folder, id, ".mp4"))
     }
     
     remote_driver$close()
 }
 
-download_video(remDr, url)
+options(timeout = max(300, getOption("timeout")))
+
+for(i in babis$ad_snapshot_url){
+    Sys.sleep(sample(2:10, 1))
+    url <- glue::glue(i)
+    cat(i)
+    download_video(remDr, url)    
+}
+
+
